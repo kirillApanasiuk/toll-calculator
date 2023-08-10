8b48e087b610873384081dcb160dad8e7836fd5c
k.apanasyuk <k.apanasyuk@RIT-W65>
2023-08-10 17:07:16 +0300

    feat: added working gui support for kafka setup

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -3,15 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	kafkaConfig "github.com/kirillApanasiuk/toll-calculator/config"
 	"github.com/kirillApanasiuk/toll-calculator/types"
+	"gopkg.in/IBM/sarama.v1"
 	"log"
 	"net/http"
 )
 
+const KafkaTopic = "obudata"
+
 func main() {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	producer, err := sarama.NewSyncProducer([]string{kafkaConfig.CONST_HOST}, config)
+	if err != nil {
+		log.Fatal("failed to initialize NewSyncProducer, err:", err)
+		return
+	}
+	defer producer.Close()
+
+	for i := 0; i < 5; i++ {
+		str := fmt.Sprint("hello kirill apanasiuk")
+		msg := &sarama.ProducerMessage{
+			Topic:     kafkaConfig.CONST_TOPIC,
+			Partition: -1,
+			Value:     sarama.StringEncoder(str),
+		}
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			log.Println("SendMessage err: ", err)
+			return
+		}
+		log.Printf("[producer] partition id: %d; offset:%d, value: %s\n", partition, offset, str)
+	}
+
+	return
 	receiver := NewDataReceiver()
 	http.HandleFunc("/ws", receiver.handleWS)
-	err := http.ListenAndServe(":3000", nil)
+	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		fmt.Println(err)
 	} else {
